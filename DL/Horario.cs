219fbe6abfb2602f0d7e9d09a0aab3aa7577c411
iespinoza96 <iespinoza@digis01.com>
2023-04-27 11:24:10 -0600

    Add project files.

---
+++ DL/Horario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DL;
+
+public partial class Horario
+{
+    public int IdHorario { get; set; }
+
+    public string? Turno { get; set; }
+
+    public int? IdAlumno { get; set; }
+
+    public virtual Alumno? IdAlumnoNavigation { get; set; }
+}
---
+++ PL/Controllers/SemestreController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class SemestreController : Controller
+    {
+        public ActionResult GetAll()
+        {
+            ML.Semestre semestre = new ML.Semestre();
+
+            ML.Result result = BL.Semestre.GetAll();
+
+            if (result.Correct)
+            {
+                semestre.Semestres = result.Objects;
+                return View(semestre);
+            }
+            else
+            {
+                ViewBag.Message = "Ocurrio un error en la consulta";
+                return View();
+            }
+            
+        }
+    }
+}
